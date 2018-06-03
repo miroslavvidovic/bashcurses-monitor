b59c1309cd12a612df06aa29cc0d316c03d255ff
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2018-06-03 19:43:34 +0200

    Define the INTEFACE inside the script

--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,14 +1,17 @@
-readonly INTERFACE="$1"
+# Wifi Interface
+INTERFACE="wlo1"
 
 get_signal(){
-if [ "$(ifconfig $INTERFACE | grep UP | wc -l) -eq 1" ]; then
-  link_quality="$(iwconfig $INTERFACE | grep Quality | cut -d'=' -f2 | cut -d' ' -f1 | cut -d'/' -f1)"
-  link_name="$(iwconfig $INTERFACE | grep ESSID | cut -d':' -f2)"
+  if [ $(ifconfig "$INTERFACE" | grep UP | wc -l) -eq 1 ]; then
+    link_quality="$(iwconfig $INTERFACE | grep Quality | cut -d'=' -f2 | cut -d' ' -f1 | cut -d'/' -f1)"
+    link_name="$(iwconfig $INTERFACE | grep ESSID | cut -d':' -f2)"
 
-  SIGNAL_QUALITY=$(($link_quality * 100 / 70))
+    SIGNAL_QUALITY=$(($link_quality * 100 / 70))
 
-  echo "[$SIGNAL_QUALITY%] on $link_name"
-fi
+    echo "[$SIGNAL_QUALITY%] on $link_name"
+  else 
+    echo "No info."
+  fi
 }
 
 get_signal
